917f814b278566db68fca2c63c52abe8f0fcf860
Sankita Sigdel <sankitasigdel@Sankitas-MacBook-Air.local>
2023-02-16 15:25:17 +0545

    modified commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+b = input("Enter a letter: ")
+letter = ""
+for i, x in enumerate(b):
+    if i % 2 == 0 :
+        letter += x.upper()
+    else:
+        letter += x.lower()
+
+print(letter)
